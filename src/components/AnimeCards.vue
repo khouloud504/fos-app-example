52f214ffe8740a2ff701c7117a3f26e5eb908180
kaoutarDJAOUD <ek_djaoud@esi.dz>
2023-06-01 15:47:26 +0100

    [DEV][08] Creating cards view

--- src/components/AnimeCards.vue
+++ src/components/AnimeCards.vue
@@ -1,13 +1,24 @@
 <template>
-  <div></div>
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-4" v-for="(anime,index) in animeList" :key="index">
+      <anime-card :anime="anime"></anime-card>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from "vuex";
+import AnimeCard from "./AnimeCard.vue";
 
-}
+export default {
+  components: {
+    AnimeCard,
+  },
+  computed: {
+    ...mapGetters(["animeList"]),
+  },
+};
 </script>
 
 <style>
-
 </style>
