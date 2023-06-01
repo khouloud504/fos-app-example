6f5becfb818a678586b7dd3f60711fbd88009e46
kaoutarDJAOUD <ek_djaoud@esi.dz>
2023-06-01 10:31:25 +0100

    [DEV][02] Creating a Navigation Bar

---
+++ src/components/layouts/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="#">
+          Home
+        </b-nav-item>
+        <b-nav-item href="#">
+          New Anime
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
