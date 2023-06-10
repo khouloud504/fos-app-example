b8a33e83b12771a264244ec4c6fb98fca9e168a4
Benammar khouloud <71606832+khouloud504@users.noreply.github.com>
2023-06-10 07:16:33 +0100

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,29 +3,38 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  animeList: []
-}
+  animeList: [],
+};
 
 const mutations = {
-  saveAnime(state, anime) {
-    state.animeList = anime
-  }
-}
+  saveAnime(state, animeList) {
+    state.animeList = animeList;
+  },
+  updateAnime(state, editedAnime) {
+    const index = state.animeList.findIndex((anime) => anime.title === editedAnime.title);
+    if (index !== -1) {
+      state.animeList.splice(index, 1, editedAnime);
+    }
+  },
+};
 
 const actions = {
   addAnime({ commit }, anime) {
-    let myAnimeList = [...state.animeList, anime]
-    commit('saveAnime', myAnimeList)
-  }
-}
+    let myAnimeList = [...state.animeList, anime];
+    commit('saveAnime', myAnimeList);
+  },
+  updateAnime({ commit }, editedAnime) {
+    commit('updateAnime', editedAnime);
+  },
+};
 
 const getters = {
   animeList: (state) => state.animeList,
+};
 
-}
 export default new Vuex.Store({
   state,
   mutations,
   actions,
-  getters
-})
+  getters,
+});
